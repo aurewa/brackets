af628814f303f13c0bc1b849c79685462e36631b
Alexander <aurewa@gmail.com>
2023-01-20 01:40:50 +0300

    feat: add implementation of check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(ex, config) {
+  
+  let openSymbols = [];
+  let closeSymbols = [];
+  
+  Object.values(config).map(x => {
+      openSymbols.push(x[0]);
+      closeSymbols.push(x[1]);
+  });
+  
+  let stack = [];
+  var triggered = false;
+
+  for (var item of ex.split('')){ 
+
+    let closeIndex = closeSymbols.indexOf(item);
+    if (closeIndex !== -1) {        
+
+      if (stack.length == 0 && item !== "|") return false;
+
+      let openIndex = openSymbols.lastIndexOf(stack[stack.length-1]);
+      if (openIndex === closeIndex){
+        stack.pop();
+        continue;
+      }
+    }
+
+    if (openSymbols.indexOf(item) !== -1){
+      stack.push(item);
+    }
+  }
+  
+  return stack.length === 0;
 }
